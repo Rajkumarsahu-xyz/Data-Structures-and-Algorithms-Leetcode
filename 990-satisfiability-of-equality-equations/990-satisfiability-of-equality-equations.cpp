6fd6cadd4315205dd6f7ff6ccd2e811608da507c
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2022-09-26 20:36:55 +0530

    Time: 11 ms (53.23%), Space: 11.3 MB (88.77%) - LeetHub

--- 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
@@ -1,19 +1,61 @@
 class Solution {
 public:
-    int uf_find(vector<int> &uf, int i) 
+    int parent[26];
+    int rank[26];
+    void makeSet()
     {
-        return uf[i] = (uf[i] == -1 || uf[i] == i) ? i : uf_find(uf, uf[i]);
+        for(int i=0; i<26; i++)
+        {
+            parent[i] = i;
+            rank[i] = 0;
+        }
     }
+    
+    int findPar(int node)
+    {
+        if(node == parent[node])
+            return node;
+        return parent[node] = findPar(parent[node]);
+    }
+    
+    void Union(int u, int v)
+    {
+        u = findPar(u);
+        v = findPar(v);
+        
+        if(rank[u] < rank[v])
+            parent[u] = v;
+        else if(rank[v] < rank[u])
+            parent[v] = u;
+        else
+            parent[v] = u, rank[u]++;
+    }
+    
     bool equationsPossible(vector<string>& equations) 
     {
-        vector<int> uf('z' + 1, -1);
-        for (auto s : equations)
-            if (s[1] == '=') 
-                uf[uf_find(uf, s[0]-'a')] = uf_find(uf, s[3]-'a');
+        makeSet();
+        int n = equations.size();
         
-        for (auto s : equations)
-            if (s[1] == '!' && uf_find(uf, s[0]-'a') == uf_find(uf, s[3]-'a')) 
-                return false;
+        for(int i=0; i<n; i++)
+        {
+            if(equations[i][1] == '=')
+                Union(equations[i][0]-'a', equations[i][3]-'a');
+        }
+        
+        for(int i=0; i<n; i++)
+        {
+            if(equations[i][1] == '=')
+            {
+                if(findPar(equations[i][0]-'a') != findPar(equations[i][3]-'a'))
+                    return false;
+            }
+            
+            else if(equations[i][1] == '!')
+            {
+                if(findPar(equations[i][0]-'a') == findPar(equations[i][3]-'a'))
+                    return false;
+            }
+        }
         
         return true;
     }
