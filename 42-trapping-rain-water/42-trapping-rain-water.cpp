1e44633da63d7583963a6add048a1d6d954aec6b
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2022-09-27 18:56:17 +0530

    Time: 24 ms (68.83%), Space: 19.7 MB (96.60%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -3,27 +3,29 @@ public:
     int trap(vector<int>& height) 
     {
         int n = height.size();
-        int prefix_mx[n];
-        int suffix_mx[n];
-        
-        int mx = 0;
-        for(int i=0; i<n; i++)
-        {
-            mx = max(mx, height[i]);
-            prefix_mx[i] = mx;
-        }
-        
-        mx = 0;
-        for(int i=n-1; i>=0; i--)
-        {
-            mx = max(mx, height[i]);
-            suffix_mx[i] = mx;
-        }
-        
         int ans = 0;
-        for(int i=0; i<n; i++)
+        
+        int l=0, r=n-1;
+        int left_mx=0, right_mx=0;
+        while(l <= r)
         {
-            ans += min(prefix_mx[i], suffix_mx[i]) - height[i];
+            if(height[l] <= height[r])
+            {
+                if(height[l] >= left_mx)
+                    left_mx = height[l];
+                else
+                    ans += left_mx - height[l];
+                l++;
+            }
+            
+            else
+            {
+                if(height[r] >= right_mx)
+                    right_mx = height[r];
+                else
+                    ans += right_mx - height[r];
+                r--;
+            }
         }
         
         return ans;
