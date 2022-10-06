a63753b93910b0c24a6152b106214fcacf535319
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2022-10-06 20:26:16 +0530

    Time: 359 ms (89.66%), Space: 130.6 MB (29.45%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.cpp
+++ 981-time-based-key-value-store/981-time-based-key-value-store.cpp
@@ -1,31 +1,18 @@
 class TimeMap {
-public:
-    
-    map<string, map<int, string>> mp;
-    TimeMap() 
-    {
-        
+public:    
+    unordered_map<string,map<int,string, greater<int>>>m;
+    TimeMap() {
     }
     
-    void set(string key, string value, int timestamp) 
-    {
-        mp[key][timestamp] = value;
+    void set(string key, string value, int timestamp) {
+        m[key][timestamp]=value;
     }
     
-    string get(string key, int timestamp) 
-    {
-        if (mp.find(key) == mp.end()) 
-        {
-            return "";
-        }
-        
-        auto itr = mp[key].upper_bound(timestamp);
-        if (itr == mp[key].begin()) 
-        {
-            return "";
-        }
-        
-        return prev(itr)->second;
+    string get(string key, int timestamp) {
+            //auto &p = m[key];
+            auto ind = m[key].lower_bound(timestamp);
+            if(ind == m[key].end()) return "";
+            return ind->second;
     }
 };
 
