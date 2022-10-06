ef45d4d3529927fc61a736cca62a0551c3860da2
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2022-10-07 00:16:23 +0530

    Time: 3 ms (94.78%), Space: 9.1 MB (21.27%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
@@ -4,12 +4,19 @@ public:
     {
         unordered_map<int, int> mp;
         int ans = 0;
+        
+        for(auto x: nums)
+        {
+            mp[x]++;
+        }
+        
         for(auto x: nums)
         {
             if(mp.find(x-diff) != mp.end()  &&  mp.find(x-(2*diff)) != mp.end())
-                ans = ans + mp[x-diff]* mp[x-(2*diff)];
-            
-            mp[x]++;
+            {
+                ans++;
+                mp[x]--, mp[x-diff]--, mp[x-(2*diff)]--;
+            }
         }
         return ans;
     }
