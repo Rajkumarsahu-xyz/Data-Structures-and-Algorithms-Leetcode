d9def76a70ffeffbda6ee20143e81f272b8dd3d8
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2022-10-07 18:12:57 +0530

    Time: 68 ms (31.96%), Space: 19.1 MB (17.65%) - LeetHub

--- 518-coin-change-ii/518-coin-change-ii.cpp
+++ 518-coin-change-ii/518-coin-change-ii.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     
-    int coinchange(int amount, vector<int>& coins,int i, vector<vector<int>>& dp)
+    int coinchange(int amount, vector<int>& coins, int i, int ctr, vector<vector<int>>& dp)
     {
         if(amount == 0)
         {
-            return 1;
+            ctr++;
+            return ctr;
         }
         
         if(amount < 0  ||  i == coins.size())
-            return 0;
+            return ctr;
         
         if(dp[i][amount] != -1)
             return dp[i][amount];
     
-        int ctr1 = coinchange(amount-coins[i], coins, i, dp);
-        int ctr2 = coinchange(amount, coins, i+1, dp);
+        int ctr1 = coinchange(amount-coins[i], coins, i, ctr, dp);
+        int ctr2 = coinchange(amount, coins, i+1, ctr, dp);
     
         return dp[i][amount] = ctr1+ctr2;
     }
@@ -28,6 +29,6 @@ public:
         
         vector<vector<int>> dp(coins.size()+1, vector<int>(amount+1, -1));
         int i=0, ctr=0;
-        return coinchange(amount, coins, i, dp);
+        return coinchange(amount, coins, i, ctr, dp);
     }
 };
