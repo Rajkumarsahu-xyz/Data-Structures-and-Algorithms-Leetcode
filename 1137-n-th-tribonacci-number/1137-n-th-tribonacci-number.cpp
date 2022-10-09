32d2c107338d3b226632c2a3b0d63c6bdcd165fe
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2022-10-09 18:35:51 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (83.68%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -8,16 +8,15 @@ public:
         if(n == 2)
             return 1;
         
-        vector<int> dp(n+1, -1);
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 1;
-        
-        for(int i=3; i<n+1; i++)
+        int prev = 1, prev2 = 1, prev3 = 0, ans;
+        for (int i = 3; i <= n; i++) 
         {
-            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+            ans = prev + prev2 + prev3;
+            prev3 = prev2;
+            prev2 = prev;
+            prev = ans;
         }
         
-        return dp[n];
+        return ans;
     }
 };
