5ba2be10d20aee7e17f5a0eb54be919322dcf4d9
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2022-10-14 19:35:47 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (28.63%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -3,24 +3,34 @@ public:
     
     int paths(int m, int n, vector<vector<int>>& dp)
     {
-        if(m < 0  ||  n < 0)
-            return 0;
+        for(int i=0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                if(i==0  &&  j==0)
+                {
+                    dp[i][j] = 1;
+                    continue;
+                }
+                
+                int right=0;
+                int bottom=0;
+                
+                if(i>0) 
+                    bottom = dp[i-1][j];
+                if(j>0)
+                    right = dp[i][j-1];
+                
+                dp[i][j] = right + bottom;
+            }
+        }
         
-        if(m == 0  &&  n == 0)
-            return 1;
-        
-        if(dp[m][n] != -1)
-            return dp[m][n];
-        
-        int right = paths(m-1, n, dp);
-        int bottom = paths(m, n-1, dp);
-        
-        return dp[m][n] = right + bottom;
+        return dp[m-1][n-1];
     }
     
     int uniquePaths(int m, int n) 
     {
-        vector<vector<int>> dp(m+1, vector<int> (n+1, -1));
-        return paths(m-1, n-1, dp);
+        vector<vector<int>> dp(m, vector<int> (n, -1));
+        return paths(m, n, dp);
     }
 };
