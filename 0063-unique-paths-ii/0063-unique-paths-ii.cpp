28701f8841ac6b91ae2ee47ff5b9e2cf64730e1b
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2022-10-14 20:53:17 +0530

    Time: 4 ms (71.63%), Space: 7.8 MB (45.83%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,27 +1,59 @@
 class Solution {
 public:
     
+//    int paths(int m, int n, vector<vector<int>>& obstacleGrid, vector<vector<int>>& dp)
+//    {
+//         if(m==0  &&  n==0)
+//             return 1;
+        
+//         if(m<0  ||  n<0)
+//             return 0;
+        
+//         if(dp[m][n] != -1)
+//             return dp[m][n];
+        
+//         int right=0;
+//         int bottom=0;
+        
+//         if(m>0  &&  obstacleGrid[m-1][n] != 1)
+//             right = paths(m-1, n, obstacleGrid, dp);
+//         if(n>0  &&  obstacleGrid[m][n-1] != 1)
+//             bottom = paths(m, n-1, obstacleGrid, dp);
+        
+//         return dp[m][n] = right+bottom;
+//    }
+        
+    
+    
+        
     int paths(int m, int n, vector<vector<int>>& obstacleGrid, vector<vector<int>>& dp)
-    {
-        if(m==0  &&  n==0)
-            return 1;
+    {        
+        for(int i=0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                if(i==0  &&  j==0)
+                {
+                    dp[i][j] = 1;
+                    continue;
+                }
+                
+                int right=0;
+                int down=0;
         
-        if(m<0  ||  n<0)
-            return 0;
+                if(i>0  &&  obstacleGrid[i-1][j] != 1)
+                    right = dp[i-1][j];
+                
+                if(j>0  &&  obstacleGrid[i][j-1] != 1)
+                    down = dp[i][j-1];
+                
+                dp[i][j] = right + down;
+            }
+        }
         
-        if(dp[m][n] != -1)
-            return dp[m][n];
-        
-        int right=0;
-        int bottom=0;
-        
-        if(m>0  &&  obstacleGrid[m-1][n] != 1)
-            right = paths(m-1, n, obstacleGrid, dp);
-        if(n>0  &&  obstacleGrid[m][n-1] != 1)
-            bottom = paths(m, n-1, obstacleGrid, dp);
-        
-        return dp[m][n] = right+bottom;
+        return dp[m-1][n-1];
     }
+    
     
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) 
     {
@@ -30,6 +62,6 @@ public:
         if(obstacleGrid[m-1][n-1] == 1) 
             return 0;
         vector<vector<int>> dp(m+1, vector<int> (n+1, -1));
-        return paths(m-1, n-1, obstacleGrid, dp);
+        return paths(m, n, obstacleGrid, dp);
     }
 };
