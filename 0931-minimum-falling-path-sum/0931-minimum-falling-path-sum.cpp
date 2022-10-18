0ee3847b970270804a88377ca7adfd3a14824adc
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2022-10-19 01:29:35 +0530

    Time: 35 ms (34.01%), Space: 10.2 MB (67.18%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,55 +1,43 @@
 class Solution {
 public:
     
-    int fallingPathSum(vector<vector<int>>& matrix, int n, vector<vector<int>>& dp)
+    int fallingPathSum(vector<vector<int>>& matrix, int n)
     {
-//         if(j < 0  ||  j > n)
-//             return 1e9;
-        
-//         if(i==n)
-//             return matrix[i][j];
-        
-//         if(dp[i][j] != -1)
-//             return dp[i][j];
-        
-//         int downsum = matrix[i][j] + fallingPathSum(matrix, n, i+1, j, dp);
-//         int leftdgsum = matrix[i][j] + fallingPathSum(matrix, n, i+1, j-1, dp); 
-//         int rightdgsum = matrix[i][j] + fallingPathSum(matrix, n, i+1, j+1, dp);
-        
-//         return dp[i][j] = min(downsum, min(rightdgsum, leftdgsum));
-        
+        vector<int> prev(n, -1);
         for(int j=0; j<n; j++)
         {
-            dp[n-1][j] = matrix[n-1][j];
+            prev[j] = matrix[n-1][j];
         }
         
         for(int i=n-2; i>=0; i--)
         {
+            vector<int> curr(n, -1);
             for(int j=n-1; j>=0; j--)
             {
-                int downsum = matrix[i][j] + dp[i+1][j];
+                int downsum = matrix[i][j] + prev[j];
                 
                 int leftdgsum = matrix[i][j];
                 if(j-1>=0)
-                    leftdgsum += dp[i+1][j-1];
+                    leftdgsum += prev[j-1];
                 else
                     leftdgsum += 1e9;
                 
                 int rightdgsum = matrix[i][j];
                 if(j+1<=n-1)
-                    rightdgsum += dp[i+1][j+1];
+                    rightdgsum += prev[j+1];
                 else
                     rightdgsum += 1e9;
                 
-                dp[i][j] = min(downsum, min(rightdgsum, leftdgsum));
+                curr[j] = min(downsum, min(rightdgsum, leftdgsum));
             }
+            prev = curr;
         }
         
         int ans = INT_MAX;
     
-        for(int j=0; j<n;j++)
+        for(int j=0; j<n; j++)
         {
-            ans = min(ans,dp[0][j]);
+            ans = min(ans, prev[j]);
         }
         
         return ans;
@@ -58,11 +46,8 @@ public:
     int minFallingPathSum(vector<vector<int>>& matrix) 
     {
         int n = matrix.size();
-        int i=0, ans = INT_MAX;
         
-        
-        vector<vector<int>> dp(n, vector<int> (n, -1));
-        return fallingPathSum(matrix, n, dp);
+        return fallingPathSum(matrix, n);
         
     }
 };
