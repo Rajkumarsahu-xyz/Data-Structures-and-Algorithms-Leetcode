73afb0bf5096061eec32152fd0abee3d2f3dcf9b
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2022-10-20 21:05:10 +0530

    Time: 1020 ms (41.44%), Space: 15.7 MB (61.56%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -1,37 +1,38 @@
 class Solution {
 public:
     
-    int fallingPathSum(vector<vector<int>>& matrix, int n, int i, int j, vector<vector<int>>& dp)
-    {   
+    int fallingPathSum(vector<vector<int>>& matrix, int n, int i, int j)
+    {
+        vector<int> prev(n, -1);
         for(int j = 0; j < n; j++)
-            dp[n-1][j] = matrix[n-1][j];
+            prev[j] = matrix[n-1][j];
         
         for(int i=n-2; i>=0; i--)
         {
+            vector<int> curr(n, -1);
             for(int j=n-1; j>=0; j--)
             {
                 int ans = INT_MAX;
                 for(int k=n-1; k>=0; k--)
                 {
                     if(k != j)
-                        ans = min(ans, dp[i+1][k]+matrix[i][j]);
+                        ans = min(ans, prev[k]+matrix[i][j]);
                 }
                 
-                dp[i][j] = ans;
+                curr[j] = ans;
             }
+            prev = curr;
         }
         
-        return *min_element(dp[0].begin(), dp[0].end());
+        return *min_element(prev.begin(), prev.end());
     }
     
     int minFallingPathSum(vector<vector<int>>& matrix) 
     {
         int n = matrix.size();
-        int i=0, j=0, ans = INT_MAX;
+        int i=0, j=0;
         
-        vector<vector<int>> dp(n, vector<int> (n, -1));
-        ans = min(ans, fallingPathSum(matrix, n, i, j, dp));
+        return fallingPathSum(matrix, n, i, j);
         
-        return ans;
     }
 };
