b5e76d5b88e87e8dc1eda53ea3e8553c0220fda4
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2022-10-27 21:03:51 +0530

    Time: 32 ms (60.28%), Space: 43.6 MB (22.20%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     
-    int targetSum(vector<int>& nums, int target, int sum, int n)
+    int targetSum(vector<int>& nums, int target, int sum, int n, vector<vector<int>>& dp)
     {
         if(n==-1 && target==sum)
             return 1;
@@ -9,12 +9,19 @@ public:
         else if(n==-1 && target!=sum)
             return 0;
         
-        return targetSum(nums, target, sum-nums[n], n-1) + targetSum(nums, target, sum+nums[n], n-1);    
+        if(dp[n][sum+1000] != -1)
+            return dp[n][sum+1000];
+        
+        int plus = targetSum(nums, target, sum+nums[n], n-1, dp);
+        int minus = targetSum(nums, target, sum-nums[n], n-1, dp);
+        
+        return dp[n][sum+1000] = plus + minus;
     }
     
     int findTargetSumWays(vector<int>& nums, int target)
     {
         int n=nums.size(), sum=0;
-        return targetSum(nums, target, sum, n-1);
+        vector<vector<int>> dp(20+1, vector<int>((2000)+1, -1));
+        return targetSum(nums, target, sum, n-1, dp);
     }
 };
