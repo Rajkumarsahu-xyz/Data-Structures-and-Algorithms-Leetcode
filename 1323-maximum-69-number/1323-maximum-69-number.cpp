8df5bbe7597ea42d14a91528e13916cb128b5c07
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2022-11-07 16:20:13 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (45.85%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.cpp
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -1,39 +1,18 @@
 class Solution {
 public:
     
-    int reverse(int n)
-    {
-        int rev = 0;
-        int a = n;
-        while(a > 0)
-        {
-            int r = a%10;
-            a /= 10;
-            rev = rev*10 + r;
-        }
-        
-        return rev;
-    }
-    
     int maximum69Number (int num) 
     {
-        int rev = reverse(num);
-        int ans = 0;
-        int a = rev;
-        int f = 0;
-        while(a > 0)
+        string s = to_string(num);
+        for(int i=0; i<s.length(); i++)
         {
-            int r = a%10;
-            if(r == 6 && f==0)
+            if(s[i] == '6')
             {
-                r = 9;
-                f = -1;
+                s[i] = '9';
+                break;
             }
-            
-            a /= 10;
-            ans = ans*10 + r;   
         }
         
-        return ans;
+        return stoi(s);
     }
 };
