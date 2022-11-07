f9fc1189e8697cd04b91623fc216ed210e5089fd
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2022-11-08 00:48:33 +0530

    Added solution - LeetHub

---
+++ Smallest window in a string containing all the characters of another string - GFG/smallest-window-in-a-string-containing-all-the-characters-of-another-string.cpp
@@ -0,0 +1,99 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find the smallest window in the string s consisting
+    //of all the characters of string p.
+    public:
+    //Function to find the smallest window in the string s consisting
+    //of all the characters of string p.
+    string smallestWindow (string s, string t)
+    {
+        int n = s.length();
+
+        unordered_map<char, int> mp(t.size());
+        int count = 0;
+        for(int i=0; i<t.length(); i++)
+        {
+            mp[t[i]]++;
+        }
+        count = mp.size();
+
+        int mn = INT_MAX;
+        int i=0, j=0;
+        int start = 0;
+        while(j<n)
+        {
+            if(mp.find(s[j])!=mp.end())
+            {
+                mp[s[j]]--;
+                if (mp[s[j]] == 0)
+                    count--;
+            }
+
+            if(count > 0)
+            j++;
+
+            else if(count == 0)
+            {
+                if(mn > j-i+1)
+                {mn = min(mn, j-i+1);
+                start = i;}
+
+                while(count==0)
+                {
+                    if(mp.find(s[i])==mp.end())
+                    {
+                        i++;
+                        if(mn > j-i+1)
+                        {mn=min(mn,j-i+1);   // updating the answer
+                        start = i;}
+                    }
+                    else
+                    {
+                        mp[s[i]]++;
+                        if(mp[s[i]]>0)
+                        {
+                            i++;
+                            count++;
+                        }
+                        else
+                        {
+                            i++;
+                            if(mn > j-i+1)
+                            {mn=min(mn,j-i+1);   // updating the answer
+                            start = i;}
+                        }
+                    }
+                }
+                j++;
+            }
+        
+        }
+
+        return mn!=INT_MAX ? s.substr(start, mn) : "-1" ;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+ {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        string pat;
+        cin>>pat;
+        Solution obj;
+        cout<<obj.smallestWindow(s, pat)<<endl;
+        
+    }
+	return 0;
+}  // } Driver Code Ends
