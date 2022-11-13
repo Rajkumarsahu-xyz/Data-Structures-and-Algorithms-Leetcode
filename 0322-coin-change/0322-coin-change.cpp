259f611a802c149b46b75ed5229cbda76e70a1ff
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2022-11-14 01:14:10 +0530

    Time: 301 ms (17.02%), Space: 49.3 MB (14.42%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -3,30 +3,35 @@ public:
     
     int change(vector<int>& coins, int n, int amount, vector<vector<int>>& dp)
     {
-        if(n == 0)
+        for(int x=0; x<=amount; x++)
         {
-            if(amount % coins[n] == 0)
-                return amount/coins[n];
+            if(x % coins[0] == 0)
+                dp[0][x] = x/coins[0];
             else
-                return 1e9;
+                dp[0][x] = 1e9;
         }
         
-        if(dp[n][amount] != -1)
-            return dp[n][amount];
+        for(int ind=1; ind<n; ind++)
+        {
+            for(int target=0; target<=amount; target++)
+            {
+                int notTake = dp[ind-1][target];
+                int take = 1e9;
+                if(coins[ind] <= target)
+                    take = 1 + dp[ind][target-coins[ind]];
+                
+                dp[ind][target] = min(notTake, take);
+            }
+        }
         
-        int notTake = change(coins, n-1, amount, dp);
-        int take = 1e9;
-        if(coins[n] <= amount)
-            take = 1 + change(coins, n, amount-coins[n], dp);
-        
-        return dp[n][amount] = min(notTake, take);
+        return dp[n-1][amount];
     }
     
     int coinChange(vector<int>& coins, int amount) 
     {
         int n = coins.size();
-        vector<vector<int>> dp(n+1, vector<int> (amount+1, -1));
-        int ans = change(coins, n-1, amount, dp);
+        vector<vector<int>> dp(n+1, vector<int> (amount+1, 0));
+        int ans = change(coins, n, amount, dp);
         
         if(ans >= 1e9)
             return -1;
