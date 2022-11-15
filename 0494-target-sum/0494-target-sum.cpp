aea120f997e1be8b2c66a232e7fc60d61dcf6068
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2022-11-15 19:49:39 +0530

    Time: 19 ms (78.66%), Space: 12.2 MB (45.14%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,27 +1,44 @@
 class Solution {
 public:
-    
-    int targetSum(vector<int>& nums, int target, int sum, int n, vector<vector<int>>& dp)
+    int targetsum(vector<int>& nums, int n, int target, vector<vector<int>>& dp)
     {
-        if(n==-1 && target==sum)
-            return 1;
+        if(n == 0)
+        {
+            if(target==0  &&  nums[0]==0)
+                return 2;
+            else if(target==0  || nums[0]==target)
+                return 1;
+            else
+                return 0;
+        }
         
-        else if(n==-1 && target!=sum)
-            return 0;
+        if(dp[n][target] != -1)
+            return dp[n][target];
         
-        if(dp[n][sum+1000] != -1)
-            return dp[n][sum+1000];
+        int notTake = 0 + targetsum(nums, n-1, target, dp);
+        int take = 0;
+        if(nums[n] <= target)
+            take = targetsum(nums, n-1, target-nums[n], dp);
         
-        int plus = targetSum(nums, target, sum+nums[n], n-1, dp);
-        int minus = targetSum(nums, target, sum-nums[n], n-1, dp);
-        
-        return dp[n][sum+1000] = plus + minus;
+        return dp[n][target] = notTake + take;
     }
     
     int findTargetSumWays(vector<int>& nums, int target)
     {
         int n=nums.size(), sum=0;
-        vector<vector<int>> dp(20+1, vector<int>((2000)+1, -1));
-        return targetSum(nums, target, sum, n-1, dp);
+        
+        for(int i=0; i<n; i++)
+        {
+            sum += nums[i];
+        }
+        
+        if(sum-target < 0) 
+            return 0;
+        if((sum-target)%2 == 1) 
+            return 0;
+        
+        int s = (sum - target)/2;
+        vector<vector<int>> dp(n+1, vector<int> (s+1, -1));
+        return targetsum(nums, n-1, s, dp);
     }
 };
