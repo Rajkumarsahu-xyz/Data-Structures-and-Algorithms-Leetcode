1b025fd7df15cce54c1639b78c66605df7b9f7c0
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2022-11-18 18:56:53 +0530

    Time: 8 ms (14.31%), Space: 5.9 MB (73.91%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,27 +1,12 @@
 class Solution {
 public:
-    int sqk(int n)
-    {
-        int a = n;
-        int sum=0;
-        while(a > 0)
-        {
-            int r = a%10;
-            a /= 10;
-            sum += r;
-        }
-        
-        return sum;
-    }
-    
     int addDigits(int num) 
     {
-        int a = num;
-        while(a > 9)
-        {
-            a = sqk(a);
-        }
-        
-        return a;
+        if(num == 0)
+            return 0;
+        if(num%9 == 0)
+            return 9;
+        else
+            return num%9;
     }
 };
