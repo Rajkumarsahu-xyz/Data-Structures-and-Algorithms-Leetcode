e485b8ad297c12a8c20ac2d5236888bf2aa14c07
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2022-11-22 17:44:16 +0530

    Time: 294 ms (55.38%), Space: 30.4 MB (67.18%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
@@ -1,30 +1,34 @@
-class Solution 
-{
+class Solution {
 public:
-    int nearestExit(vector<vector<char>>& maze, vector<int>& e)
+    int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) 
     {
         int m = maze.size(), n = maze[0].size();
-        int dirs[5] = {0, -1, 0, 1, 0};
-
-        auto is_exit = [&](int i, int j) -> int
-        {
-            return (i!=e[0] || j!=e[1]) && (i*j==0 or i==m-1 or j==n-1);
-        };
+        vector<pair<int, int>> dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
         
-        deque<array<int,3>> dq = {{e[0],e[1],0}};
+        queue<array<int, 3>> q;
+        q.push({entrance[0], entrance[1], 0});
+        maze[entrance[0]][entrance[1]] = '+';
         
-        while (!dq.empty())
+        int ctr = 0;
+        
+        while(!q.empty())
         {
-            auto [i,j,s] = dq.front(); dq.pop_front();
+            auto [i, j, currDis] = q.front();
+            q.pop();
             
-            for (int d = 0; d < 4; ++d)
+            for(auto x : dirs)
             {
-                int ii = i + dirs[d], jj = j + dirs[d + 1];
-                if (ii < 0 or ii >= m or jj < 0 or jj >= n or maze[ii][jj] == '+')
-                    continue;
-                maze[ii][jj] = '+';
-                if (is_exit(ii,jj)) return s+1;
-                dq.push_back({ii,jj,s+1});
+                int nextRow = i + x.first, nextCol = j + x.second;
+                if (0 <= nextRow && nextRow < m && 0 <= nextCol && nextCol < n && maze[nextRow][nextCol] == '.') 
+                {
+                    if (nextRow==0 || nextRow==m-1 || nextCol==0 || nextCol==n-1)
+                    {
+                        return currDis+1;
+                    }
+                    
+                    maze[nextRow][nextCol] = '+';
+                    q.push({nextRow, nextCol, currDis+1});
+                }
             }
         }
         
