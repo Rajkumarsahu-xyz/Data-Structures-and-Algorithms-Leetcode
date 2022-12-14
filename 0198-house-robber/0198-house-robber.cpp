a01d917692b1ddc5fa06d38f76a780b06c706784
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2022-12-14 12:26:54 +0530

    Time: 3 ms (56.21%), Space: 7.9 MB (25.79%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
     
-    int houseRob(int n, vector<int>& nums, vector<int>& dp)
+    int robber(vector<int>& nums, int n, vector<int>& dp)
     {
-        if(n < 0)
+        if(n<0)
             return 0;
-        if(n == 0)
-            return nums[n];
-        
         if(dp[n] != -1)
             return dp[n];
         
-        int rob = houseRob(n-2, nums, dp) + nums[n];
-        int notRob = houseRob(n-1, nums, dp);
+        int notrobb = 0 + robber(nums, n-1, dp);
+        int robb = nums[n] + robber(nums, n-2, dp);
         
-        return dp[n] = max(rob, notRob);
+        return dp[n] = max(notrobb, robb);
     }
     
     int rob(vector<int>& nums) 
     {
         int n = nums.size();
         vector<int> dp(n+1, -1);
-        return houseRob(n-1, nums, dp);
+        return robber(nums, n-1, dp);
     }
 };
