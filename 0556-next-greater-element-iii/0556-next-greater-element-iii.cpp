3a96fd03727bf51dc28365f9aa247da7bfa1a642
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2022-12-17 19:29:17 +0530

    Time: 0 ms (100.00%), Space: 6 MB (26.90%) - LeetHub

---
+++ 0556-next-greater-element-iii/0556-next-greater-element-iii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int nextGreaterElement(int n) 
+    {
+        string s = to_string(n);
+        next_permutation(s.begin(), s.end());
+        long long ans = stoll(s);
+        
+        if(ans > INT_MAX  || ans <= n)
+            return -1;
+        else 
+            return ans;
+    }
+};
