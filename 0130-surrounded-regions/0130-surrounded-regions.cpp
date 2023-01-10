481040887eb0ab899413b2b9ab3acdbcd7c6fc65
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2023-01-10 23:07:18 +0530

    Time: 19 ms (61.59%), Space: 10.5 MB (32.59%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    void dfs(vector<vector<char>>& board, int row, int col, vector<vector<int>>& visited)
+    {
+        visited[row][col] = 1;
+        
+        int n = board.size();
+        int m = board[0].size();
+        int delrow[] = {-1, 0, 1, 0};
+        int delcol[] = {0, 1, 0, -1}; 
+        
+        for(int i = 0;i<4;i++) 
+        {
+            int nrow = row + delrow[i];
+            int ncol = col + delcol[i];
+
+            if(nrow>=0  &&  nrow<n  &&  ncol>=0  &&  ncol<m  &&  !visited[nrow][ncol]  &&  board[nrow][ncol]=='O') 
+            {
+                dfs(board, nrow, ncol, visited);
+            }
+        }
+    }
+    
+    void solve(vector<vector<char>>& board) 
+    {
+        int n = board.size(), m = board[0].size();
+        vector<vector<int>> visited(n, vector<int>(m, 0));
+        for(int i=0; i<n; i++)
+	    {
+            if(!visited[i][0]  &&  board[i][0]=='O')
+            {
+                dfs(board, i, 0, visited);
+            }
+            
+            if(!visited[i][m-1]  &&  board[i][m-1]=='O')
+            {
+                dfs(board, i, m-1, visited);
+            }
+	    }
+        
+        for(int j=0; j<m; j++)
+        {
+            if(!visited[0][j]  &&  board[0][j]=='O')
+            {
+                dfs(board, 0, j, visited);
+            }
+            
+            if(!visited[n-1][j]  &&  board[n-1][j]=='O')
+            {
+                dfs(board, n-1, j, visited);
+            }
+        }
+        
+        
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<m; j++)
+            {
+                if(!visited[i][j]  &&  board[i][j]=='O')
+                {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+    }
+};
