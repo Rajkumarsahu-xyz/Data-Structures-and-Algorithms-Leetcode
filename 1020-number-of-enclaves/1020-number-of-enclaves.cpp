4630a69806820848cb0eb909cccc35cdd5b86d17
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2023-01-13 17:20:29 +0530

    Time: 81 ms (76.98%), Space: 27.7 MB (81.69%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& board, int row, int col, vector<vector<int>>& visited)
+    void bfs(vector<vector<int>>& board, int n, int m, queue<pair<int, int>>& q, vector<vector<int>>& visited)
     {
-        visited[row][col] = 1;
-        
-        int n = board.size();
-        int m = board[0].size();
         int delrow[] = {-1, 0, 1, 0};
         int delcol[] = {0, 1, 0, -1}; 
-        
-        for(int i = 0;i<4;i++) 
+        while(!q.empty())
         {
-            int nrow = row + delrow[i];
-            int ncol = col + delcol[i];
-
-            if(nrow>=0  &&  nrow<n  &&  ncol>=0  &&  ncol<m  &&  !visited[nrow][ncol]  &&  board[nrow][ncol]==1) 
+            auto x = q.front();
+            q.pop();
+            int row = x.first;
+            int col = x.second;
+            for(int i=0; i<4; i++)
             {
-                dfs(board, nrow, ncol, visited);
+                int nrow = row + delrow[i];
+                int ncol = col + delcol[i];
+
+                if(nrow>=0  &&  nrow<n  &&  ncol>=0  &&  ncol<m  &&  !visited[nrow][ncol]  &&  board[nrow][ncol]==1) 
+                {
+                    visited[nrow][ncol] = 1;
+                    q.push({nrow, ncol});
+                }
             }
         }
     }
@@ -28,25 +31,20 @@ public:
         vector<vector<int>> visited(n, vector<int>(m, 0));
         
         int ones=0;
-        // for(int i=0; i<n; i++)
-        // {
-        //     for(int j=0; j<m; j++)
-        //     {
-        //         if(board[i][j] == 1)
-        //             ones++;
-        //     }
-        // }
+        queue<pair<int, int>> q;
         
         for(int i=0; i<n; i++)
 	    {
             if(!visited[i][0]  &&  board[i][0]==1)
             {
-                dfs(board, i, 0, visited);
+                q.push({i, 0});
+                visited[i][0] = 1;
             }
             
             if(!visited[i][m-1]  &&  board[i][m-1]==1)
             {
-                dfs(board, i, m-1, visited);
+                q.push({i, m-1});
+                visited[i][m-1] = 1;
             }
 	    }
         
@@ -56,14 +54,18 @@ public:
         {
             if(!visited[0][j]  &&  board[0][j]==1)
             {
-                dfs(board, 0, j, visited);
+                q.push({0, j});
+                visited[0][j] = 1;
             }
             
             if(!visited[n-1][j]  &&  board[n-1][j]==1)
             {
-                dfs(board, n-1, j, visited);
+                q.push({n-1, j});
+                visited[n-1][j] = 1;
             }
         }
+        
+        bfs(board, n, m, q, visited);
         
         for(int i=0; i<n; i++)
         {
