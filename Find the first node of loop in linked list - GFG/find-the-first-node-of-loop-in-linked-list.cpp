92dbd0fa734b4ca80366e3de69aded0f2e647278
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2023-01-18 11:00:49 +0530

    Added solution - LeetHub

---
+++ Find the first node of loop in linked list - GFG/find-the-first-node-of-loop-in-linked-list.cpp
@@ -0,0 +1,118 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void loopHere(Node* head, Node* tail, int position)
+{
+    if(position==0) return;
+    
+    Node* walk = head;
+    for(int i=1; i<position; i++)
+        walk = walk->next;
+    tail->next = walk;
+}
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+/*struct Node
+{
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+
+*/
+class Solution
+{
+    public:
+     //Function to find first node if the linked list has a loop.
+    int findFirstNode(Node* head)
+    {
+        Node* p = head, *q = head;
+        int ans = -1;
+        while(p && q && q->next)
+        {
+            p = p->next;
+            q = q->next->next;
+            if(p == q)
+            {
+                ans = p->data;
+                break;
+            }
+        }
+        
+        if(ans != -1)
+        {
+            if(p == head)
+            {
+                ans = head->data;
+            }
+            
+            else
+            {
+                p = head;
+                while(p->next != q->next)
+                {
+                    p = p->next;
+                    q = q->next;
+                }
+                
+                ans = q->next->data;
+            }
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, num;
+        cin>>n;
+        
+        Node *head, *tail;
+        cin>> num;
+        head = tail = new Node(num);
+        
+        for(int i=0 ; i<n-1 ; i++)
+        {
+            cin>> num;
+            tail->next = new Node(num);
+            tail = tail->next;
+        }
+        
+        int pos;
+        cin>> pos;
+        loopHere(head,tail,pos);
+        
+        Solution ob;
+        int ans = ob.findFirstNode(head);
+        cout<<ans<<"\n";
+    }
+	return 0;
+}
+// } Driver Code Ends
