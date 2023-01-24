0c477d354ba710c9143868b53ea80f6c735b5599
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2023-01-24 18:15:46 +0530

    Time: 3 ms (91.11%), Space: 12.3 MB (84.37%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -13,36 +13,43 @@ class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) 
     {
-        vector<vector<int>> ans;
-        queue<pair<TreeNode*, pair<int, int>>> q;
-        map<int, map<int, multiset<int>>> mp;
-        q.push({root, {0, 0}});
-        while(!q.empty())
+        queue<pair<int,TreeNode*> > q;
+        vector<vector<int> > ans;
+        q.push({0,root});
+        map<int,vector<int> > mp;
+        mp[0].push_back(root->val);
+        
+        while(q.size())
         {
-            int n = q.size();
-            for(int i=0; i<n; i++)
+            queue<pair<int,TreeNode*> > tq;
+            while(q.size())
             {
-                TreeNode* t = q.front().first;
-                int x = q.front().second.first;
-                int y = q.front().second.second;
+                TreeNode* node= q.front().second;
+                int level=q.front().first;
+                if(node->right) tq.push({level+1,node->right});
+                if(node->left) tq.push({level-1,node->left});
                 q.pop();
-                mp[x][y].insert(t->val);
-                if(t->left)
-                    q.push({t->left, {x-1, y+1}});
-                if(t->right)
-                    q.push({t->right, {x+1, y+1}});
+            }
+            q=tq;
+            vector<pair<int,int> > hor_level;
+            while(tq.size())
+            {
+                hor_level.push_back({tq.front().first,tq.front().second->val});
+                tq.pop();
+            }
+            
+            sort(hor_level.begin(),hor_level.end());
+            for(auto itr:hor_level)
+            {
+                mp[itr.first].push_back(itr.second);
             }
         }
         
-        for(auto x : mp)
+        for(auto itr:mp)
         {
-            vector<int> v;
-            for(auto y : x.second)
-            {
-                v.insert(v.end(), y.second.begin(), y.second.end());
-            }
-            ans.push_back(v);
+            ans.push_back(itr.second);
         }
+        
         return ans;
     }
 };
