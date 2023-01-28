f8759c9b0bdf2aa993fd98177b1fb42004d7c668
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2023-01-28 18:39:52 +0530

    Time: 28 ms (66.59%), Space: 13.6 MB (62.11%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,19 +1,44 @@
 class Solution {
 public:
-    bool dfs(int node, int color, vector<int>& colored, vector<vector<int>>& graph)
-    {
-        colored[node] = color;
+//     bool dfs(int node, int color, vector<int>& colored, vector<vector<int>>& graph)
+//     {
+//         colored[node] = color;
         
-        for(auto x : graph[node])
+//         for(auto x : graph[node])
+//         {
+//             if(colored[x] == -1)
+//             {
+//                 if(!dfs(x, !color, colored, graph))
+//                     return false;
+//             }
+            
+//             else if(colored[x] == color)
+//                 return false;
+//         }
+        
+//         return true;
+//     }
+    
+    bool bfs(int start, int n, vector<vector<int>>& graph, vector<int>& colored)
+    {
+        queue<int> q;
+        q.push(start);
+        colored[start] = 0;
+        
+        while(!q.empty())
         {
-            if(colored[x] == -1)
+            int node = q.front();
+            q.pop();
+            for(auto x : graph[node])
             {
-                if(!dfs(x, !color, colored, graph))
+                if(colored[x] == -1)
+                {
+                    colored[x] = !colored[node];
+                    q.push(x);
+                }
+                else if(colored[x] == colored[node])
                     return false;
             }
-            
-            else if(colored[x] == color)
-                return false;
         }
         
         return true;
@@ -25,16 +50,27 @@ public:
         int n = graph.size();
         vector<int> colored(n, -1);
         
-        //colored[0] = 0;
         for(int i=0; i<n; i++)
         {
             if(colored[i] == -1)
             {
-                if(!dfs(i, 0, colored, graph))
+                if(!bfs(i, n, graph, colored))
                     return false;
             }
         }
         
         return true;
+        
+        //colored[0] = 0;
+//         for(int i=0; i<n; i++)
+//         {
+//             if(colored[i] == -1)
+//             {
+//                 if(!dfs(i, 0, colored, graph))
+//                     return false;
+//             }
+//         }
+        
+//         return true;
     }
 };
