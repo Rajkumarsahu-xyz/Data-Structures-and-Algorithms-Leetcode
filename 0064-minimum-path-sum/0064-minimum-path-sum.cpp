3f7a1f0b2e4d3f6bab432d64b41c43734e970506
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2023-02-05 11:14:29 +0530

    Time: 10 ms (73.87%), Space: 10.1 MB (58.52%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,42 +1,31 @@
 class Solution {
 public:
-    
-    int pathSum(int m, int n, vector<vector<int>>& grid, vector<int>& prev)
-    {
-        for(int i=0; i<m; i++)
-        {
-            vector<int> curr(n, -1);
-            for(int j=0; j<n; j++)
-            {
-                if(i == 0  &&  j == 0)
-                   curr[0] = grid[0][0];
-                
-                else
-                {
-                    int upsum = grid[i][j];
-                    if(i>0) upsum += prev[j];
-                    else upsum += 1e9;
-                
-                    int leftsum = grid[i][j];
-                    if(j>0) leftsum += curr[j-1];
-                    else leftsum += 1e9;
-                
-                    curr[j] = min(upsum , leftsum);
-                }
-            }
-            
-            prev = curr;
-        }
-        
-        return prev[n-1];
-    }
-    
     int minPathSum(vector<vector<int>>& grid) 
     {
-        int m = grid.size();
-        int n = grid[0].size();
+        int n=grid.size();
+        int m=grid[0].size();
         
-        vector<int> prev(n, -1);
-        return pathSum(m, n, grid, prev);
+        vector<vector<int>> dp(n, vector<int>(m, 0));
+        dp[0][0]=grid[0][0];
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(i==0&&j!=0)
+                {
+                    dp[i][j] = min(INT_MAX, dp[i][j-1]+grid[i][j]);
+                }
+                else if(i!=0&&j==0)
+                {
+                    dp[i][j] = min(dp[i-1][j]+grid[i][j], INT_MAX);
+                }
+                else if(i!=0&&j!=0)
+                {
+                    dp[i][j] = min(dp[i-1][j]+grid[i][j], dp[i][j-1]+grid[i][j]);
+                }
+            }
+        }
+        return dp[n-1][m-1];
     }
 };
