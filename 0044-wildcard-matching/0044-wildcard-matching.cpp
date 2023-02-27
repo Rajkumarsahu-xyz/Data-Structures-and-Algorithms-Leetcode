3419b0edd4d3c0436bf868839b6083f58964e1d7
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2023-02-27 21:04:10 +0530

    Time: 61 ms (64.24%), Space: 28 MB (6.83%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,47 +1,85 @@
 class Solution {
 public:
     
+//     bool lcs(string &word1, string &word2, int n1, int n2, vector<vector<int>>& dp)
+//     {
+//         if(n1 < 0  &&  n2 < 0)
+//             return true;
+        
+//         if(n1<0)
+//         {
+//             // return 0;
+//             // return (word2[n2] == '*' && lcs(word1, word2, n1, n2-1, dp));
+//             while(n2 >= 0)
+//             {
+//                 if(word2[n2] != '*')
+//                     return false;
+//                 n2--;
+//             }
+//             return 1;
+//         }
+        
+//         if(n2<0) return 0;
+        
+//         if(n1>=0 && dp[n1][n2] != -1)
+//         {
+//             return dp[n1][n2];
+//         }
+        
+//         bool notTake = false, one=false, two=false, three=false;
+        
+//         if(word2[n2]=='?' || (n1>=0 && word1[n1] == word2[n2]))
+//             return dp[n1][n2] = lcs(word1, word2, n1-1, n2-1, dp);
+        
+//         else if(word2[n2] == '*')
+//         {
+//             one = lcs(word1, word2, n1, n2-1, dp);
+//             two = lcs(word1, word2, n1-1, n2, dp);
+//             three = lcs(word1, word2, n1-1, n2-1, dp);
+            
+//             notTake = (one || two || three);
+//         }
+//         if(n1<0) return notTake;
+//         return dp[n1][n2] = notTake;
+        
+//     }
+    
     bool lcs(string &word1, string &word2, int n1, int n2, vector<vector<int>>& dp)
     {
         if(n1 < 0  &&  n2 < 0)
             return true;
         
-        if(n1<0)
+        else if(n1 < 0)
         {
-            // return 0;
-            // return (word2[n2] == '*' && lcs(word1, word2, n1, n2-1, dp));
             while(n2 >= 0)
             {
                 if(word2[n2] != '*')
                     return false;
                 n2--;
             }
-            return 1;
+            return true;
         }
         
-        if(n2<0) return 0;
-        
-        if(n1>=0 && dp[n1][n2] != -1)
+        else if(n2 < 0)
         {
+            return false;
+        }
+        
+        if(dp[n1][n2] != -1)
             return dp[n1][n2];
-        }
         
-        bool notTake = false, one=false, two=false, three=false;
+        bool take = false, notTake = false;
         
-        if(word2[n2]=='?' || (n1>=0 && word1[n1] == word2[n2]))
-            return dp[n1][n2] = lcs(word1, word2, n1-1, n2-1, dp);
+        if(word1[n1] == word2[n2] || word1[n1]=='?' || word2[n2]=='?')
+            take = lcs(word1, word2, n1-1, n2-1, dp);
         
-        else if(word2[n2] == '*')
+        else
         {
-            one = lcs(word1, word2, n1, n2-1, dp);
-            two = lcs(word1, word2, n1-1, n2, dp);
-            three = lcs(word1, word2, n1-1, n2-1, dp);
-            
-            notTake = (one || two || three);
+            if(word2[n2] == '*')
+                notTake = (lcs(word1, word2, n1-1, n2, dp) || lcs(word1, word2, n1, n2-1, dp) || lcs(word1, word2, n1-1, n2-1, dp));
         }
-        if(n1<0) return notTake;
-        return dp[n1][n2] = notTake;
         
+        return dp[n1][n2] = take || notTake;
     }
     
     bool isMatch(string s, string p) 
