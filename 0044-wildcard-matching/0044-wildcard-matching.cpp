95908b4d2a6606efc68b3899bb82e5839a5d29bb
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2023-02-27 21:01:59 +0530

    Time: 72 ms (53.59%), Space: 28 MB (6.83%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -6,10 +6,9 @@ public:
         if(n1 < 0  &&  n2 < 0)
             return true;
         
-        if(n2<0) return 0;
-        
         if(n1<0)
         {
+            // return 0;
             // return (word2[n2] == '*' && lcs(word1, word2, n1, n2-1, dp));
             while(n2 >= 0)
             {
@@ -20,23 +19,27 @@ public:
             return 1;
         }
         
-        if(dp[n1][n2] != -1)
+        if(n2<0) return 0;
+        
+        if(n1>=0 && dp[n1][n2] != -1)
+        {
             return dp[n1][n2];
+        }
         
         bool notTake = false, one=false, two=false, three=false;
         
-        if((word1[n1] == word2[n2]) || word2[n2]=='?')
+        if(word2[n2]=='?' || (n1>=0 && word1[n1] == word2[n2]))
             return dp[n1][n2] = lcs(word1, word2, n1-1, n2-1, dp);
         
         else if(word2[n2] == '*')
         {
             one = lcs(word1, word2, n1, n2-1, dp);
-            if(n1>=0)
-                two = lcs(word1, word2, n1-1, n2, dp);
-            if(n1>=0)
-                three = lcs(word1, word2, n1-1, n2-1, dp);
+            two = lcs(word1, word2, n1-1, n2, dp);
+            three = lcs(word1, word2, n1-1, n2-1, dp);
+            
             notTake = (one || two || three);
         }
+        if(n1<0) return notTake;
         return dp[n1][n2] = notTake;
         
     }
