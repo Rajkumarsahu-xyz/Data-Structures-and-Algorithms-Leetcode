94a08484a9be47720600f774ba81c3e026023163
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2023-03-10 00:21:33 +0530

    Added solution - LeetHub

--- Find anagrams in linked list - GFG/find-anagrams-in-linked-list.cpp
+++ Find anagrams in linked list - GFG/find-anagrams-in-linked-list.cpp
@@ -52,114 +52,114 @@ You can also use the following for printing the link list.
 printList(Node* node);
 */
 
-// class Solution {
-//   public:
-//     vector<Node*> findAnagrams(struct Node* head, string s) 
-//     {
-//         vector<Node*> ans;
-        
-//         unordered_map<char, int> mp, mp1;
-//         for(int i=0; i<s.length(); i++)
-//             mp[s[i]]++;
-//         int ctr = mp.size();
-//         mp1 = mp;
-        
-//         Node* i = head;
-//         Node* j = head;
-//         while(j != NULL)
-//         {
-//             mp[j->data]--;
-//             if(mp[j->data] == 0)
-//             {
-//                 // cout << "Raj1" <<  " ";
-//                 // cout << endl;
-//                 ctr--;
-//                 // j = j->next;
-//             }
-//             if(ctr == 0)
-//             {
-//                 // cout << "Raj2" <<  " ";
-//                 // cout << endl;
-//                 Node* temp = j;
-//                 j = j->next;
-//                 temp->next = NULL;
-                
-//                 // Node* temp = j->next;
-//                 // j->next = NULL;
-//                 ans.push_back(i);
-//                 i = j;
-//                 // j = temp;
-//                 ctr = mp1.size();
-//                 mp = mp1;
-//                 continue;
-//                 // i = j->next;
-//                 // j = j->next;
-//             }
-//             if(mp[j->data] < 0)
-//             {
-//                 // cout << "Raj3" <<  " ";
-//                 // cout << endl;
-//                 while(mp[j->data] < 0)
-//                 {
-//                     mp[i->data]++;
-//                     if(mp[i->data]>0) ctr++;
-//                     i = i->next;
-//                 }
-//                 // j = j->next;
-//             }
-            
-//             j = j->next;
-//         }
-        
-//         return ans;
-//     }
-// };
-
-
-
-
-
-
-
-
-
 class Solution {
   public:
-    vector<Node*> findAnagrams(struct Node* head, string s) {
+    vector<Node*> findAnagrams(struct Node* head, string s) 
+    {
+        vector<Node*> ans;
         
-        vector<Node*> res;
-        vector<int> window(26), slidingWindow(26);
+        unordered_map<char, int> mp, mp1;
+        for(int i=0; i<s.length(); i++)
+            mp[s[i]]++;
+        int ctr = mp.size();
+        mp1 = mp;
         
-        for(auto &x: s)window[x-'a']++;
-        Node *start = head, *end = head;
-        int count = 0;
-        
-        while(end != NULL) {
-            slidingWindow[end->data - 'a']++;
-            count++;
-            
-            if(slidingWindow == window) {
-                res.push_back(start);
-                start = end->next;
-                end->next = NULL;
-                end = start;
-                count = 0;
-                
-                for(int i = 0; i < 26; i++)slidingWindow[i] = 0;
+        Node* i = head;
+        Node* j = head;
+        while(j != NULL)
+        {
+            mp[j->data]--;
+            if(mp[j->data] == 0)
+            {
+                // cout << "Raj1" <<  " ";
+                // cout << endl;
+                ctr--;
+                // j = j->next;
             }
-            else {
-                end = end->next;
+            if(ctr == 0)
+            {
+                // cout << "Raj2" <<  " ";
+                // cout << endl;
+                Node* temp = j;
+                j = j->next;
+                temp->next = NULL;
                 
-                if(count >= s.size()) {
-                    slidingWindow[start->data - 'a']--;
-                    start = start->next;
+                // Node* temp = j->next;
+                // j->next = NULL;
+                ans.push_back(i);
+                i = j;
+                // j = temp;
+                ctr = mp1.size();
+                mp = mp1;
+                continue;
+                // i = j->next;
+                // j = j->next;
+            }
+            if(mp[j->data] < 0)
+            {
+                // cout << "Raj3" <<  " ";
+                // cout << endl;
+                while(mp[j->data] < 0)
+                {
+                    mp[i->data]++;
+                    if(mp[i->data]==1) ctr++;
+                    i = i->next;
                 }
+                // j = j->next;
             }
+            
+            j = j->next;
         }
         
-        return res;
+        return ans;
     }
 };
+
+
+
+
+
+
+
+
+
+// class Solution {
+//   public:
+//     vector<Node*> findAnagrams(struct Node* head, string s) {
+        
+//         vector<Node*> res;
+//         vector<int> window(26), slidingWindow(26);
+        
+//         for(auto &x: s)window[x-'a']++;
+//         Node *start = head, *end = head;
+//         int count = 0;
+        
+//         while(end != NULL) {
+//             slidingWindow[end->data - 'a']++;
+//             count++;
+            
+//             if(slidingWindow == window) {
+//                 res.push_back(start);
+//                 start = end->next;
+//                 end->next = NULL;
+//                 end = start;
+//                 count = 0;
+                
+//                 for(int i = 0; i < 26; i++)slidingWindow[i] = 0;
+//             }
+//             else {
+//                 end = end->next;
+                
+//                 if(count >= s.size()) {
+//                     slidingWindow[start->data - 'a']--;
+//                     start = start->next;
+//                 }
+//             }
+//         }
+        
+//         return res;
+//     }
+// };
 
 
 //{ Driver Code Starts.
