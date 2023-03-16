4b9b6aab04c6724f327d5aa80449a47a55542f50
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2023-03-16 23:16:35 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.cpp
+++ Topological sort - GFG/topological-sort.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 // } Driver Code Ends
+
 class Solution
 {
 	public:
@@ -43,6 +44,61 @@ class Solution
 	    return topo;
 	}
 };
+
+
+// class Solution
+// {
+// 	public:
+// 	//Function to return list containing vertices in Topological order. 
+// // 	void dfs(int start, int V, vector<int> adj[], stack<int>& stk, vector<int>& visited)
+// // 	{
+// // 	    visited[start] = 1;
+// // 	    for(auto x : adj[start])
+// // 	    {
+// // 	        if(visited[x] == 0)
+// // 	        {
+// // 	            dfs(x, V, adj, stk, visited);
+// // 	        }
+// // 	    }
+	    
+// // 	    stk.push(start);
+// // 	}
+	
+// 	vector<int> topoSort(int V, vector<int> adj[]) 
+// 	{
+// 	    vector<int> kahn, indegree(V);
+// 	    queue<int> q;
+// 	    for(int i=0; i<V; i++)
+// 	    {
+// 	        for(auto x : adj[i])
+// 	        {
+// 	            indegree[x]++;
+// 	        }
+// 	    }
+	    
+// 	    for(int i=0; i<V; i++)
+// 	    {
+// 	        if(indegree[i] == 0)
+// 	        {
+// 	            q.push(i);
+// 	        }
+// 	    }
+	    
+// 	    while(!q.empty())
+// 	    {
+// 	        int node = q.front();
+// 	        q.pop();
+// 	        kahn.push_back(node);
+// 	        for(auto x : adj[node])
+// 	        {
+// 	            indegree[x]--;
+// 	            if(indegree[x] == 0) q.push(x);
+// 	        }
+// 	    }
+	    
+// 	    return kahn;
+// 	}
+// };
 
 //{ Driver Code Starts.
 
