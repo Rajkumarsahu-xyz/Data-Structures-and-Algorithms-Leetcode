2beff6316904ac116626c1f582a1fcc986f27492
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2023-03-16 16:12:08 +0530

    Time: 19 ms (6.39%), Space: 14.1 MB (37.85%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,8 +1,43 @@
+// class Solution {
+// public:
+//     void dfs(vector<vector<int>>& image, int row, int col, int starting_color, vector<vector<int>>& ans, int color, vector<int> delrow, vector<int> delcol)
+//     {
+//         ans[row][col] = color;
+//         int n = image.size();
+//         int m = image[0].size();
+        
+//         for(int i=0; i<4; i++)
+//         {
+//             int nrow = row + delrow[i];
+//             int ncol = col + delcol[i];
+//             if((nrow>=0 && nrow<n)  &&  (ncol>=0 && col<m)  &&  image[nrow][ncol] == starting_color  &&  ans[nrow][ncol] != color)
+//             {
+//                 dfs(image, nrow, ncol, starting_color, ans, color, delrow, delcol);
+//             }
+//         }
+//     }
+    
+//     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) 
+//     {
+//         vector<vector<int>> ans = image;
+//         int starting_color = image[sr][sc];
+        
+//         vector<int> delrow = {-1, 0, 1, 0};
+//         vector<int> delcol = {0, -1, 0, 1};
+//         dfs(image, sr, sc, starting_color, ans, color, delrow, delcol);
+//         return ans;
+//     }
+// };
+
+
+
+
 class Solution {
 public:
-    void dfs(vector<vector<int>>& image, int row, int col, int starting_color, vector<vector<int>>& ans, int color, vector<int> delrow, vector<int> delcol)
+    void dfs(vector<vector<int>>& image, int row, int col, int starting_color, int color, vector<int> delrow, vector<int> delcol)
     {
-        ans[row][col] = color;
+        // cout << row << " " << col << endl;
+        image[row][col] = color;
         int n = image.size();
         int m = image[0].size();
         
@@ -10,21 +45,23 @@ public:
         {
             int nrow = row + delrow[i];
             int ncol = col + delcol[i];
-            if((nrow>=0 && nrow<n)  &&  (ncol>=0 && col<m)  &&  image[nrow][ncol] == starting_color  &&  ans[nrow][ncol] != color)
+            if((nrow>=0 && nrow<n)  &&  (ncol>=0 && ncol<m)  &&  image[nrow][ncol] == starting_color  &&  image[nrow][ncol] != color)
             {
-                dfs(image, nrow, ncol, starting_color, ans, color, delrow, delcol);
+                dfs(image, nrow, ncol, starting_color, color, delrow, delcol);
             }
         }
     }
     
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) 
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) 
     {
-        vector<vector<int>> ans = image;
+        // vector<vector<int>> ans = image;
         int starting_color = image[sr][sc];
         
+        // cout << sr << " " << sc << endl;
         vector<int> delrow = {-1, 0, 1, 0};
         vector<int> delcol = {0, -1, 0, 1};
-        dfs(image, sr, sc, starting_color, ans, color, delrow, delcol);
-        return ans;
+        dfs(image, sr, sc, starting_color, newColor, delrow, delcol);
+        return image;
     }
+    
 };
