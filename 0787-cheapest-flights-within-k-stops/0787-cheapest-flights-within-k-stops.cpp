544477b2c925e3cd0b1cefa3390b9f3fbb50f4d8
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2023-03-24 17:36:26 +0530

    Time: 20 ms (93.76%), Space: 13 MB (90.08%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,30 +1,46 @@
+#define pi pair<int, pair<int, int>>
+
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<pair<int, int>>> adj(n);
-        for (auto& e : flights) {
-            adj[e[0]].push_back({e[1], e[2]});
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) 
+    {
+        vector<pair<int, int>> adjlist[n];
+        for(int i=0; i<flights.size(); i++)
+        {
+            adjlist[flights[i][0]].push_back({flights[i][1], flights[i][2]});
         }
-        vector<int> dist(n, numeric_limits<int>::max());
-        queue<pair<int, int>> q;
-        q.push({src, 0});
-        int stops = 0;
-
-        while (stops <= k && !q.empty()) {
-            int sz = q.size();
-            // Iterate on current level.
-            while (sz--) {
-                auto [node, distance] = q.front();
-                q.pop();
-                // Iterate over neighbors of popped node.
-                for (auto& [neighbour, price] : adj[node]) {
-                    if (price + distance >= dist[neighbour]) continue;
-                    dist[neighbour] = price + distance;
-                    q.push({neighbour, dist[neighbour]});
+        
+        priority_queue<pi, vector<pi>, greater<pi>> pq; // {stops, {node, distance covered}}
+        vector<int> distance(n+1, 1e9);
+        distance[src] = 0;
+        pq.push({0, {src, 0}});
+        
+        while(!pq.empty())
+        {
+            int stops = pq.top().first;
+            int city = pq.top().second.first;
+            int dis_till_now = pq.top().second.second;
+            pq.pop();
+            
+            if(stops > k)
+                continue;
+            
+            for(auto x : adjlist[city])
+            {
+                int adjcity = x.first;
+                int dis_to_city = x.second;
+                
+                if(dis_till_now + dis_to_city  <  distance[adjcity])
+                {
+                    distance[adjcity] = dis_till_now + dis_to_city;
+                    pq.push({stops+1, {adjcity, distance[adjcity]}});
                 }
             }
-            stops++;
         }
-        return dist[dst] == numeric_limits<int>::max() ? -1 : dist[dst];
+        
+        if (distance[dst] == 1e9)
+            return -1;
+        
+        return distance[dst];
     }
 };
