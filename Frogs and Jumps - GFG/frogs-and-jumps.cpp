f66ca1506d38a3e069280f8f92305979431969e9
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2023-03-27 00:06:39 +0530

    Added solution - LeetHub

--- Frogs and Jumps - GFG/frogs-and-jumps.cpp
+++ Frogs and Jumps - GFG/frogs-and-jumps.cpp
@@ -8,65 +8,32 @@ using namespace std;
 
 class Solution {
   public:
-    // int unvisitedLeaves(int N, int leaves, int frogs[]) 
-    // {
-    //     vector<int> v(leaves+1, 0);
-    //     for(int i=0; i<N; i++)
-    //     {
-    //         int k = frogs[i];
-    //         for(int j=0; j<leaves+1; j+=k)
-    //         {
-    //             if(j < leaves+1)
-    //                 v[j] = 1;
-    //         }
-    //         int f = 0;
-    //         for(int i=0; i<leaves+1; i++)
-    //         {
-    //             if(v[i] == 0)
-    //                 f = 1;
-    //         }
-    //         if(f == 0)
-    //             break;
-    //     }
-        
-    //     int ctr = 0;
-    //     for(int i=0; i<leaves+1; i++)
-    //     {
-    //         if(v[i] == 0)
-    //             ctr++;
-    //     }
-        
-    //     return ctr;
-    // }
-    
-    
-    
-    
-    int unvisitedLeaves(int n, int tl, int a[]) {
-        // Code l
-        
-        vector<int> l(tl + 1, 0);
-        int ul = 0;
-        unordered_set<int> st;
-        for(int i = 0; i < n; i++){
-            int curr_ele = 0;
-            
-            // if remove this conditio then will not pass 5 test case. 
-            // it tells, if any same number comes up again then, continue. 
-            if(st.count(a[i]) == 0) st.insert(a[i]);
-            else continue;
-            while(curr_ele <= tl){
-                curr_ele += a[i];
-                
-                if(curr_ele <= tl && l[curr_ele] == 0 ){
-                    l[curr_ele] = 1;
-                    ul++;
+    int unvisitedLeaves(int N, int leaves, int frogs[]) 
+    {
+        vector<int> visited(leaves+1, 0);
+        for(int i=0; i<N; i++)
+        {
+            int k = frogs[i];
+            if(k<=leaves && !visited[k])
+            {
+                for(int j=0; j<leaves+1; j+=k)
+                {
+                    visited[j] = 1;
                 }
             }
         }
         
-        return tl - ul;
+        int ctr = 0;
+        for(int i=0; i<leaves+1; i++)
+        {
+            if(visited[i] == 0)
+                ctr++;
+        }
+        
+        return ctr;
     }
+    
+    
 };
 
 
