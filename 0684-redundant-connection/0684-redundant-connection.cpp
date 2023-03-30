d6c1f6ac92d55c651b9c60780b6caf5f6f06a9ab
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2023-03-31 00:45:54 +0530

    Time: 6 ms (83.13%), Space: 9.1 MB (37.57%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -0,0 +1,83 @@
+class DisjointSet 
+{
+    public:
+    vector<int> rank, parent, size;
+    DisjointSet(int n) 
+    {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) 
+        {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findUPar(int node) 
+    {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    bool unionByRank(int u, int v) 
+    {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v)
+            return false;
+        
+        if (rank[ulp_u] < rank[ulp_v]) 
+        {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) 
+        {
+            parent[ulp_v] = ulp_u;
+        }
+        else 
+        {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+        
+        return true;
+    }
+
+    void unionBySize(int u, int v) 
+    {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) 
+        {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else 
+        {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+
+
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) 
+    {
+        int n = edges.size();
+        DisjointSet ds(n+1);
+        
+        for(auto x : edges)
+        {
+            if(!ds.unionByRank(x[0], x[1]))
+                return x;
+        }
+        
+        return {};
+    }
+};
