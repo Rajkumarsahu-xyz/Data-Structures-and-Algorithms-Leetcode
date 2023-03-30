21cb600195ae7fc28f7289a6656057526ff85f17
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2023-03-30 16:47:57 +0530

    Added solution - LeetHub

--- Disjoint set (Union-Find) - GFG/disjoint-set-union-find.cpp
+++ Disjoint set (Union-Find) - GFG/disjoint-set-union-find.cpp
@@ -36,15 +36,17 @@ int main(){
 
 class DisjointSet 
 {
-    vector<int> rank, parent;
+    vector<int> rank, parent, size;
     public:
     DisjointSet(int n) 
     {
         rank.resize(n + 1, 0);
         parent.resize(n + 1);
+        size.resize(n + 1);
         for (int i = 0; i <= n; i++) 
         {
             parent[i] = i;
+            size[i] = 1;
         }
     }
 
@@ -72,6 +74,23 @@ class DisjointSet
         {
             parent[ulp_v] = ulp_u;
             rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v) 
+    {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) 
+        {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else 
+        {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
         }
     }
 };
