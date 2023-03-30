ad4659214eab11b2cfe875ada0a216748aea142a
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2023-03-30 19:20:53 +0530

    Time: 22 ms (84.57%), Space: 13.8 MB (63.98%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,47 +1,91 @@
-class Solution {
-public:
+class DisjointSet 
+{
+    public:
+    vector<int> rank, parent, size;
     
-    void dfs(vector<int> adjlist[], int node, vector<int>& visited, vector<int>& ans)
+    DisjointSet(int n) 
     {
-        visited[node] = 1;
-        ans.push_back(node);
-        for(auto x : adjlist[node])
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) 
         {
-            if(!visited[x])
-            {
-                dfs(adjlist, x, visited, ans);
-            }
+            parent[i] = i;
+            size[i] = 1;
         }
     }
-    
-    
+
+    int findUPar(int node) 
+    {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) 
+    {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) 
+        {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) 
+        {
+            parent[ulp_v] = ulp_u;
+        }
+        else 
+        {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v) 
+    {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) 
+        {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else 
+        {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+
+class Solution {
+public:
     int findCircleNum(vector<vector<int>>& isConnected) 
     {
-        int V = isConnected.size();
-        vector<int> ans, visited(V, 0);
-        vector<int> adjlist[V];
+        int n = isConnected.size();
+        DisjointSet ds(n);
         
-        for(int i=0; i<V; i++)
+        for(int i=0; i<n; i++)
         {
-            for(int j=0; j<V; j++)
+            for(int j=0; j<n; j++)
             {
-                if(isConnected[i][j]==1 && i!=j)
+                if(isConnected[i][j] == 1)
                 {
-                    adjlist[i].push_back(j);
-                    adjlist[j].push_back(i);
+                    ds.unionBySize(i, j);
                 }
             }
         }
         
         int ctr = 0;
-        for(int i=0; i<V; i++)
+        for(int i=0; i<n; i++)
         {
-            if(!visited[i])
-            {
+            if(ds.parent[i] == i)
                 ctr++;
-                dfs(adjlist, i, visited, ans);
-            }
         }
+        
         return ctr;
     }
 };
