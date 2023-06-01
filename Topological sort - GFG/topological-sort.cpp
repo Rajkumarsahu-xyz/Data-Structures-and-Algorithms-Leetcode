e3569ca28e6017e8db90c4d51571f34c5c51790b
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2023-06-01 21:50:19 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.cpp
+++ Topological sort - GFG/topological-sort.cpp
@@ -4,101 +4,38 @@ using namespace std;
 
 // } Driver Code Ends
 
+
 class Solution
 {
-	public:
-	//Function to return list containing vertices in Topological order. 
-	void dfs(int start, int V, vector<int> adj[], stack<int>& stk, vector<int>& visited)
-	{
-	    visited[start] = 1;
-	    for(auto x : adj[start])
-	    {
-	        if(visited[x] == 0)
-	        {
-	            dfs(x, V, adj, stk, visited);
-	        }
-	    }
-	    
-	    stk.push(start);
-	}
-	
-	vector<int> topoSort(int V, vector<int> adj[]) 
-	{
-	    vector<int> topo, visited(V);
-	    stack<int> stk;
-	    
-	    for(int i=0; i<V; i++)
-	    {
-	        if(visited[i] == 0)
-	        {
-	            dfs(i, V, adj, stk, visited);
-	        }
-	    }
-	    
-	    while(!stk.empty())
-	    {
-	        topo.push_back(stk.top());
-	        stk.pop();
-	    }
-	    
-	    return topo;
-	}
+    public:
+    void topo(int node, vector<int>&vis, stack<int>& st, vector<int> adj[]){
+        vis[node]=1;
+        for(auto it: adj[node]){
+            if(!vis[it]){
+                topo(it,vis,st,adj);
+            }
+        }
+        st.push(node);
+    }
+    
+    vector<int> topoSort(int N, vector<int> adj[]) 
+    {
+        stack<int> st;
+        vector<int> vis(N,0);
+        for(int i=0;i<N;i++){
+            if(vis[i]==0){
+                topo(i,vis,st,adj);
+            }
+        }
+        
+        vector<int> ans;
+        while(!st.empty()){
+            ans.push_back(st.top());
+            st.pop();
+        }
+        return ans;
+    }
 };
-
-
-// class Solution
-// {
-// 	public:
-// 	//Function to return list containing vertices in Topological order. 
-// // 	void dfs(int start, int V, vector<int> adj[], stack<int>& stk, vector<int>& visited)
-// // 	{
-// // 	    visited[start] = 1;
-// // 	    for(auto x : adj[start])
-// // 	    {
-// // 	        if(visited[x] == 0)
-// // 	        {
-// // 	            dfs(x, V, adj, stk, visited);
-// // 	        }
-// // 	    }
-	    
-// // 	    stk.push(start);
-// // 	}
-	
-// 	vector<int> topoSort(int V, vector<int> adj[]) 
-// 	{
-// 	    vector<int> kahn, indegree(V);
-// 	    queue<int> q;
-// 	    for(int i=0; i<V; i++)
-// 	    {
-// 	        for(auto x : adj[i])
-// 	        {
-// 	            indegree[x]++;
-// 	        }
-// 	    }
-	    
-// 	    for(int i=0; i<V; i++)
-// 	    {
-// 	        if(indegree[i] == 0)
-// 	        {
-// 	            q.push(i);
-// 	        }
-// 	    }
-	    
-// 	    while(!q.empty())
-// 	    {
-// 	        int node = q.front();
-// 	        q.pop();
-// 	        kahn.push_back(node);
-// 	        for(auto x : adj[node])
-// 	        {
-// 	            indegree[x]--;
-// 	            if(indegree[x] == 0) q.push(x);
-// 	        }
-// 	    }
-	    
-// 	    return kahn;
-// 	}
-// };
 
 //{ Driver Code Starts.
 
