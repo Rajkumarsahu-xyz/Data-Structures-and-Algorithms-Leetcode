4213497e54f631cf173295987abe502fe6a2492a
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2023-06-03 19:39:21 +0530

    Time: 21 ms (29.80%), Space: 7.3 MB (8.76%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -3,7 +3,7 @@ public:
     int characterReplacement(string s, int k) 
     {
         int n = s.length();
-        unordered_map<int, int> mp;
+        unordered_map<char, int> mp;
         
         int i=0, j=0;
         int mx = 0;
@@ -18,38 +18,32 @@ public:
                 flip = s[j];
             }
             
-            // ans = max(ans, j-i);
             
-            if((j-i+1)-mx == k)
+            
+            if((j-i+1)-mx >= k)
             {
-                ans = max(ans, j-i+1);
-                // mp[s[i]]--;
-                // if(mp[s[i]] == 0)
-                //     mp.erase(s[i]);
-                // i++;
-            }
-            
-            
-            if((j-i+1)-mx > k)
-            {
-                ans = max(ans, j-i);
-                mp[s[i]]--;
-                if(mp[s[i]] == 0)
-                    mp.erase(s[i]);
-                i++;
+                if((j-i+1)-mx == k)
+                {
+                    ans = max(ans, j-i+1);
+                }
+                
+                else
+                {
+                    ans = max(ans, j-i);
+                    mp[s[i]]--;
+                    if(mp[s[i]] == 0)
+                        mp.erase(s[i]);
+                    i++;
+                }
             }
             
             else
             {
                 ans = max(ans, j-i+1);
             }
+            
             j++;
         }
-        
-        // if((j-i)-mx > k)
-        // {
-        //     ans = max(ans, j-i+1);
-        // }
         
         return ans;
     }
