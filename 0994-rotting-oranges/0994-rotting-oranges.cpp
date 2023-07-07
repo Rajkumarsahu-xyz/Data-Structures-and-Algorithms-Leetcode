48fd9795b3ed90ccfe5415d28bc451f505a9dd76
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2023-07-07 10:55:14 +0530

    Time: 3 ms (95.72%), Space: 13.1 MB (71.83%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -2,62 +2,60 @@ class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) 
     {
-        int n = grid.size();
-        int m = grid[0].size();
+        int m = grid.size();
+        int n = grid[0].size();
         
         queue<pair<pair<int, int>, int>> q;
-        vector<vector<int>> visited = grid;
-        
-        for (int i = 0; i < n; i++) 
+        // vector<vector<int>> visited(m, vector<int>(n));
+        for(int i=0; i<m; i++)
         {
-            for (int j = 0; j < m; j++) 
+            for(int j=0; j<n; j++)
             {
-              if (grid[i][j] == 2) 
-              {
-                  q.push({{i, j}, 0}); 
-                  visited[i][j] = 2;
-              }
-              else if(grid[i][j] == 1)
-                  visited[i][j] = 1;
-              
-              else
-                  visited[i][j] = 0;
-            }
-        }
-        
-        int time = 0;
-        int drow[] = {-1, 0, +1, 0};
-        int dcol[] = {0, 1, 0, -1}; 
-
-        while (!q.empty()) 
-        {
-            int r = q.front().first.first;
-            int c = q.front().first.second;
-            int t = q.front().second;
-            time = max(time, t);
-            q.pop();
-            for (int i=0; i<4; i++) 
-            {
-                int nrow = r + drow[i];
-                int ncol = c + dcol[i];
-
-                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && visited[nrow][ncol]!=2 && grid[nrow][ncol]==1) 
+                if(grid[i][j] == 2)
                 {
-                    q.push({{nrow, ncol}, t+1});
-                    visited[nrow][ncol] = 2;
+                    q.push({{i, j}, 0});
                 }
             }
+        
         }
         
-        for (int i = 0; i < n; i++) 
+        vector<int> dir = {-1, 0, 1, 0, -1};
+        int ans = 0;
+        while(!q.empty())
         {
-            for (int j = 0; j < m; j++) 
+            int x = q.front().first.first;
+            int y = q.front().first.second;
+            int minutes = q.front().second;
+            ans = max(ans, minutes);
+            
+            q.pop();
+            int nx, ny;
+            for(int i=0; i<4; i++)
             {
-              if (visited[i][j] == 1)
-                  return -1;
+                nx = x+dir[i];
+                ny = y+dir[i+1];
+                
+                if(nx>=0 && nx<m && ny>=0 && ny<n && grid[nx][ny]==1)
+                {
+                    grid[nx][ny] = 2;
+                    
+                    q.push({{nx, ny}, minutes+1});
+                }
+                    
+            }
+            
+        }
+        
+        for(int i=0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                if(grid[i][j] == 1)
+                    return -1;
             }
         }
         
-        return time;
+        return ans;
+        
     }
 };
