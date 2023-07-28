d912fce3bc818babe97c5859a1693b0e90507d2a
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2023-07-28 21:04:28 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (55.32%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
      
-    int predict(vector<int>& nums, int i, int j)
+    int predict(vector<int>& nums, int i, int j, vector<vector<int>> &dp)
     {
         if(i > j)
            return 0;
         if(i == j)
             return nums[i];
         
-        int curr_score = max((nums[i] + min(predict(nums, i+2, j), predict(nums, i+1, j-1))) , (nums[j] + min(predict(nums, i, j-2), predict(nums, i+1, j-1))));
+        if(dp[i][j] != -1)
+            return dp[i][j];
         
-        return curr_score;
+        int curr_score = max((nums[i] + min(predict(nums, i+2, j, dp), predict(nums, i+1, j-1, dp))) , (nums[j] + min(predict(nums, i, j-2, dp), predict(nums, i+1, j-1, dp))));
+        
+        return dp[i][j] = curr_score;
     }
     
     bool PredictTheWinner(vector<int>& nums) 
@@ -26,7 +29,8 @@ public:
             sum += nums[i];
         }
         
-        int score1 = predict(nums, 0, n-1);
+        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+        int score1 = predict(nums, 0, n-1, dp);
         int score2 = sum-score1;
         
         return score1>=score2;
