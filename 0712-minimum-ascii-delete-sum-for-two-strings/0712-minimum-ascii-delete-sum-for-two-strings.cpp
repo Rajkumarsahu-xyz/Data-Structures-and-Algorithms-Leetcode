84d099ac09e21d6f54eba3977e8623e8abda3a63
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2023-08-01 00:42:37 +0530

    Time: 67 ms (37.20%), Space: 15.4 MB (7.16%) - LeetHub

---
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+    
+    int dead_end_sum(string &s , int i) 
+    {
+        int sum = 0;
+        for( ; i<s.length() ; i++)
+            sum+=int(s[i]);
+        
+        return sum;
+    }
+    
+    int sub(string &a, string &b, int i, int j)
+    {
+        int n = a.length();
+        int m = b.length();
+        int sum = 0;
+        if(i==n || j==m)
+        {
+            if(i==n and j==m)   return 0;
+            return (i==n) ? dead_end_sum(b,j) : dead_end_sum(a,i);
+        }
+        
+        if(dp[i][j] != -1)
+            return dp[i][j];
+    
+        if(a[i] == b[j])
+            sum = sub(a,b,i+1,j+1);
+        else
+        {
+            sum = min({ sub(a,b,i+1,j) + int(a[i]) ,sub(a,b,i,j+1) + int(b[j])});
+        }      
+        
+        return dp[i][j] = sum;
+    }
+    
+    int minimumDeleteSum(string a, string b) 
+    {
+        dp = vector<vector<int>>(a.length()+1, vector<int>(b.length()+1 , -1));
+        return sub(a,b,0,0);
+    }
+};
