e804502c233238367170fc6fd0a5a726b41a1ff4
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2023-08-02 04:48:54 +0530

    Time: 20 ms (86.66%), Space: 9.9 MB (50.16%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,32 +1,59 @@
+// class Solution {
+// public:
+    
+//     void combo(vector<int> &nums, int i, int n, vector<int> v, vector<vector<int>> &ans, int k)
+//     {
+//         if(v.size() == k)
+//         {
+//             ans.push_back(v);
+//             return;
+//         }
+        
+//         v.push_back(nums[i]);
+//         combo(nums, i+1, n, v, ans, k);
+//         v.pop_back();
+//         combo(nums, i+1, n, v, ans, k);
+//     }
+    
+//     vector<vector<int>> combine(int n, int k) 
+//     {
+//         vector<int> nums;
+//         for(int i=1; i<=n; i++)
+//         {
+//             nums.push_back(i);
+//         }
+        
+//         vector<vector<int>> ans;
+//         vector<int> v;
+//         combo(nums, 0, n, v, ans, k);
+        
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
-    void combinations(vector<int>& nums, int i, int n, vector<int>& comb, vector<vector<int>>& ans, int k)
+    vector<vector<int>> ans;
+    
+    void helper(int idx, int k,vector<int>&current,int n)
     {
-        if(comb.size() == k)
+        if(current.size()==k)    // base case
         {
-            ans.push_back(comb);
+            ans.push_back(current);
             return;
         }
         
-        for(int j=i; j<n; j++)
+        for(int i=idx;i<n+1;i++)
         {
-            comb.push_back(nums[j]);
-            combinations(nums, j+1, n, comb, ans, k);
-            comb.pop_back();
+            current.push_back(i);  //consider the current element i
+            helper(i+1,k,current,n); // generate combinations
+            current.pop_back(); //proceed to next element
         }
     }
     
-    vector<vector<int>> combine(int n, int k)
-    {
-        vector<int> nums;
-        for(int i=0; i<n; i++)
-        {
-            nums.push_back(i+1);
-        }
-        vector<vector<int>> ans;
-        vector<int> comb;
-        
-        combinations(nums, 0, n, comb, ans, k);
-        return ans;
+    vector<vector<int>> combine(int n, int k) {
+        vector<int>current;
+        helper(1,k,current,n);
+        return ans; //return answer
     }
 };
