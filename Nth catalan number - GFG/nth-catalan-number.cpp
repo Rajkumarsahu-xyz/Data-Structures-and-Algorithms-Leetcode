6c4f2885b2f65111d4376e62677b372de8150957
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2023-08-16 23:52:04 +0530

    Added solution - LeetHub

---
+++ Nth catalan number - GFG/nth-catalan-number.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to find the nth catalan number.
+    int findCatalan(int n) 
+    {
+        //code here
+        if(n==0)return 1;
+        int mod=1e9+7;
+        vector<long long>ans(n+1,0);
+        
+        ans[0]=1;
+        ans[1]=1;
+        for(int i=2;i<=n;i++){
+            for(int j=0;j<i;j++){
+                ans[i]=(ans[i] + (ans[j]*ans[i-j-1])%mod)%mod; //for factorial 
+            }
+        }
+        
+        
+        return ans[n];
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{
+	int t;
+	cin>>t;
+	while(t--) {
+	    
+	    int n;
+	    cin>>n;
+	    Solution obj;
+	    cout<< obj.findCatalan(n) <<"\n";    
+	}
+	return 0;
+}
+// } Driver Code Ends
