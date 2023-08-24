d72f14e006d5bcdb49a02457ca10397b927f4f46
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2023-08-25 03:06:05 +0530

    Time: 4 ms (32.56%), Space: 6.3 MB (63.01%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.cpp
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -1,42 +1,58 @@
 class Solution {
 public:
-    string reorganizeString(string s) {
-        vector<int> charCounts(26, 0);
-        for (char c : s) {
-            charCounts[c - 'a']++;
+    string reorganizeString(string s) 
+    {
+        int n = s.length();
+        string ans;
+        priority_queue<pair<int, char>> pq;
+        unordered_map<char, int> mp;
+        
+        int mx = INT_MIN;
+        for(int i=0; i<n; i++)
+        {
+            mp[s[i]]++;
+            // mx = max(mx, mp[s[i]]);
         }
-        int maxCount = 0, letter = 0;
-        for (int i = 0; i < charCounts.size(); i++) {
-            if (charCounts[i] > maxCount) {
-                maxCount = charCounts[i];
-                letter = i;
+        
+        for(auto x : mp)
+        {
+            pq.push({x.second, x.first});
+        }
+        
+        
+        // cout << 5 + 'a' << endl;
+        
+        while(!pq.empty())
+        {
+            int freq = pq.top().first;
+            char key = pq.top().second;
+            pq.pop();
+            
+            if(ans.length() == 0 || ans[ans.length()-1] != key)
+            {
+                ans += key;
+                freq--;
+                if(freq)
+                    pq.push({freq, key});
+            }
+            
+            else if(!pq.empty())
+            {
+                int freq2 = pq.top().first;
+                int key2 = pq.top().second;
+                pq.pop();
+                
+                ans += key2;
+                freq2--;
+                if(freq2)
+                    pq.push({freq2, key2});
+                pq.push({freq, key});
+            }
+            else
+            {
+                return "";
             }
         }
-        if (maxCount > (s.length() + 1) / 2) {
-            return "";
-        }
-        string ans = s;
-        int index = 0;
-
-        // Place the most frequent letter
-        while (charCounts[letter] != 0) {
-            ans[index] = char(letter + 'a');
-            index += 2;
-            charCounts[letter]--;
-        }
-
-        // Place rest of the letters in any order
-        for (int i = 0; i < charCounts.size(); i++) {
-            while (charCounts[i] > 0) {
-                if (index >= s.length()) {
-                    index = 1;
-                }
-                ans[index] = char(i + 'a');
-                index += 2;
-                charCounts[i]--;
-            }
-        }
-
         return ans;
     }
 };
