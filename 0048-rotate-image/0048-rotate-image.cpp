8bf68fbc1973b6b515508ef7c2636f50160885d9
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2023-08-26 16:05:07 +0530

    Time: 4 ms (36.19%), Space: 7.2 MB (16.84%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -3,7 +3,7 @@ public:
     void rotate(vector<vector<int>>& matrix) 
     {
         int n=matrix.size(), m=matrix[0].size();
-        reverse(matrix.begin(), matrix.end());
+        // reverse(matrix.begin(), matrix.end());
         for(int i=0; i<n; i++)
         {
             for(int j=i+1; j<m; j++)
@@ -11,5 +11,11 @@ public:
                 swap(matrix[i][j], matrix[j][i]);
             }
         }
+        
+        for(int i=0; i<n; i++)
+        {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+        
     }
 };
