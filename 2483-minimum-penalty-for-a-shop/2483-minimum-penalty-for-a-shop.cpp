ec2963f7c972358bea79858d0d5c6e78c659c1c1
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2023-08-30 04:18:41 +0530

    Time: 24 ms (80.16%), Space: 10.7 MB (72.63%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -1,36 +1,80 @@
+// class Solution {
+// public:
+    
+//     bool isfeasible(int mid, string customers, int n, int mn)
+//     {
+//         int ctr = 0;
+//         for(int i=0; i<n; i++)
+//         {
+//             if(i<mid && customers[i]=='N')
+//             {
+//                 ctr++;
+//             }
+//             else if(i>=mid && customers[i]=='Y')
+//             {
+//                 ctr++;
+//             }
+//         }
+        
+//         return (ctr<mn);
+//     }
+    
+//     int bestClosingTime(string customers)
+//     {
+//         int n = customers.length();
+        
+//         int i=0, j=n-1;
+//         int mn = INT_MAX, ans = INT_MAX;
+        
+//         while(i<=j)
+//         {
+//             int mid = i + (j-i)/2;
+//             if(isfeasible(mid, customers, n, mn))
+//             {
+//                 ans = i;
+//             }
+            
+//             else
+//         }
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
 public:
-    int bestClosingTime(string customers) 
-    {
-        int n = customers.size();
-        vector<int> lpre(n+1), rpre(n+1);
-        int l=0, r=0;
-        
-        int i=0, j=n-1; 
-        for(; i<n && j>=0; i++, j--)
-        {
-            lpre[i] = l;
-            if(customers[i] == 'N')
-                l++;
-           
-            rpre[j+1] = r;
-            if(customers[j] == 'Y')
-                r++;
-        }
-        
-        lpre[i] = l;
-        rpre[j+1] = r;
-        
-        int mn = INT_MAX, ans=0;
-        for(int i=0; i<n+1; i++)
-        {
-            if(lpre[i]+rpre[i] < mn)
-            {
-                mn = lpre[i]+rpre[i];
-                ans = i;
+    int bestClosingTime(string customers) {
+        // Start with closing at hour 0 and assume the current penalty is 0.
+        int minPenalty = 0, curPenalty = 0;
+        int earliestHour = 0;
+
+        for (int i = 0; i < customers.size(); i++) {
+            char ch = customers[i];
+            
+            // If status in hour i is 'Y', moving it to open hours decrement
+            // penalty by 1. Otherwise, moving 'N' to open hours increment
+            // penatly by 1.
+            if (ch == 'Y') {
+                curPenalty--;
+            } else {
+                curPenalty++;
+            }
+
+            // Update earliestHour if a smaller penatly is encountered.
+            if (curPenalty < minPenalty) {
+                earliestHour = i + 1;
+                minPenalty = curPenalty;
             }
         }
-        
-        return ans;
+
+        return earliestHour;
     }
 };
