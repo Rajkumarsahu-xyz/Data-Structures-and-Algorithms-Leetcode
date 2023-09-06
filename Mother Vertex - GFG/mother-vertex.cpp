7d46955ac3e4ec376d633886a6298ebab30241d3
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2023-09-07 00:55:20 +0530

    Added solution - LeetHub

--- Mother Vertex - GFG/mother-vertex.cpp
+++ Mother Vertex - GFG/mother-vertex.cpp
@@ -8,7 +8,7 @@ class Solution
 {
     public:
      
-    void DFS(vector<int>adj[],int node, vector<int>&vis  )
+    void DFS(vector<int>adj[],int node, vector<int>&vis)
     {
        vis[node]=1;
        for(auto x: adj[node])
@@ -19,15 +19,16 @@ class Solution
            }
        }
     }
-    
+     
 	int findMotherVertex(int V, vector<int>adj[])
 	{
 	    int ans=0;
-	     vector<int>vis(V,0);
+	    vector<int>vis(V,0);
 	    for(int i=0;i<V;i++)
-	    { 
-	        if(!vis[i]){
-	          DFS(adj,i,vis );
+	    {
+	        if(!vis[i])
+	        {
+	          DFS(adj,i,vis);
 	          ans=i;
 	        }
 	        
