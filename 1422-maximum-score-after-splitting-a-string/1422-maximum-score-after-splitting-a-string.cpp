69aa58d9fd493f32e5e9baa8dc53b9b08ce44863
Raj Kumar Sahu <77824474+Rajkumarsahu-xyz@users.noreply.github.com>
2023-12-22 22:25:46 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (30.36%) - LeetHub

---
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int maxScore(string s) 
+    {
+        int n = s.length();
+        vector<int> left(n, 0), right(n, 0);
+        
+        if(s[0] == '0')
+            left[0] = 1;
+        if(s[n-1] == '1')
+            right[n-1] = 1;
+        
+        for(int i=1; i<n; i++) {
+            if(s[i] == '0') {
+                left[i] = left[i-1] + 1;
+            }
+            else {
+                left[i] = left[i-1];
+            }
+        }
+        
+        for(int i=n-2; i>=0; i--) {
+            if(s[i] == '1') {
+                right[i] = right[i+1] + 1;
+            }
+            else {
+                right[i] = right[i+1];
+            }
+        }
+        
+        int mx = 0;
+        for(int i=0; i<n-1; i++) {
+            // cout << left[i] << " " << right[i+1] << endl;
+            mx = max(mx, left[i] + right[i+1]);
+        }
+        
+        return mx;
+    }
+};
